fbf2d67fefffeecb44697005d0fd710776ae250e
Sean Dinan <seancdinan@gmail.com>
2016-02-29 11:07:20 -0500

    Simple 'make' function to simplify js DOM element creation down to one line.

--- tby.js
+++ tby.js
@@ -1,15 +1,27 @@
 // A simple library to abstract and simplify chunks of javascript that I'm frequently writing.
-
 var TBY = {};
 
-TBY.test = function() {
-	console.log('This is test.')
+// THINGS THAT ANNOY ME AND SHOULD BE ABSTRACTED:
+// creating elements:
+// normal way: var name = document.creatElement('type');
+// name.id = 'name';
+// parent.appendChild(name);
+
+TBY.make = function(type, name, properties, inner) {
+	// Creates a doc element of type 'type' with id 'name' and
+	// (optional) style properties defined by the properties object
+	// e.g. {height: 100px, width: 400px}
+	// (optional) innerHTML 'inner' for text and whatnot.
+	this.element = document.createElement(type);
+	this.element.id = name;
+	if (properties != undefined){
+		for (var i in properties){
+			this.element.style[i]  = properties[i];
+		}
+	}
+	if (inner != undefined){
+		this.element.innerHTML = inner;
+	}
+	return this.element
 };
 
-TBY.makeDiv = function(x, y, color){
-	var box = document.createElement('div');
-	box.style.height = y + 'px';
-	box.style.width = x + 'px';
-	box.style.backgroundColor = color;
-	return box
-};
