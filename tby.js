22fed936a5d4bec94e1ea547e80347618970749c
Sean Dinan <seancdinan@gmail.com>
2016-03-02 09:35:58 -0500

    Removed excess comments

--- tby.js
+++ tby.js
@@ -1,12 +1,6 @@
 // A simple library to abstract and simplify chunks of javascript that I'm frequently writing.
 var TBY = {};
 
-// THINGS THAT ANNOY ME AND SHOULD BE ABSTRACTED:
-// creating elements:
-// normal way: var name = document.creatElement('type');
-// name.id = 'name';
-// parent.appendChild(name);
-
 TBY.make = function(type, name, properties, inner) {
 	// Creates a doc element of type 'type' with id 'name' and
 	// (optional) style properties defined by the properties object
